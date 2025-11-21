8709ade090eae515c78e2ca1024205f201a7921c
Đinh Hoàng Quân <hoangquan1357968@gmail.com>
2025-11-22 01:25:11 +0700

    Initial commit: Project skeleton setup with Spring Boot and Docker

---
+++ src/main/java/com/lightvision/simple_product_api/SimpleProductApiApplication.java
@@ -0,0 +1,13 @@
+package com.lightvision.simple_product_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleProductApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleProductApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lightvision/simple_product_api/SimpleProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lightvision.simple_product_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleProductApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
